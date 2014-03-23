1263088500d9f00eaf582264969d411f092bafdd
kaoriprice <kaori.price@gmail.com>
2014-03-23 10:32:08 -0700

    Add JSON

--- js/eileen.js
+++ js/eileen.js
@@ -2,22 +2,20 @@ $(function () {
 
 	var container = $("#container");
 
-
 	var templateHtml = $("#portfolioItemTemplate").html();
 	var templateFunction = Handlebars.compile(templateHtml);
 
-	$.each(portfolio__item, function (index, item) {
+	$.get("data/portfolio.json", function (portfolio__item) {
+		$.each(portfolio__item, function (index, item) {
+			item.index = index;
+			var outputHtml = templateFunction(item);
+			container.append(outputHtml);
+		});
 
-		var outputHtml = templateFunction(item);
-
-		container.append(outputHtml);
-
+		new AnimOnScroll( container[0], {
+			minDuration : 0.4,
+			maxDuration : 0.7,
+			viewportFactor : 0.2
+		} );
 	});
-
-	new AnimOnScroll( container[0], {
-		minDuration : 0.4,
-		maxDuration : 0.7,
-		viewportFactor : 0.2
-	} );
-
 });
