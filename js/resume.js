284e506de55e2a6141d22a012d1cbe997b88fd97
kaoriprice <kaori.price@gmail.com>
2015-01-06 21:38:33 -0800

    about page complete

--- js/resume.js
+++ js/resume.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-	var navWidth = $(".resume__nav").width();
-	var containerWidth = $(".resume").width()  -15;
-	$(".resume__container").css("width", containerWidth);
-	$(".resume__container").css("padding-left", navWidth);
+// $(document).ready(function(){
+// 	var navWidth = $(".resume__nav").width();
+// 	var containerWidth = $(".resume").width();
+// 	$(".resume__container").css("width", containerWidth);
+// 	$(".resume__container").css("padding-left", navWidth);
 
-});
+// });
